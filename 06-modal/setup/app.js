07faf70e066771c9c39ee2f36ecc87bb60c58cb7
iamkaizen <kimsokhom654@gmail.com>
2024-11-16 22:28:37 +0700

    Project 06 is completed successfully

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,20 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+//step 1
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+//step 2
+modalBtn.addEventListener('click', function(){
+    //step 3
+    modalOverlay.classList.add('open-modal');
+});
+
+//step 2
+closeBtn.addEventListener('click', function(){
+    //step 4
+    modalOverlay.classList.remove('open-modal');
+});
